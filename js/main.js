228b31e96656b1a406e94b6df2c0d8fbdc2d4896
Joy-NaNa <1031518951@qq.com>
2022-02-23 17:12:50 +0800

    橘子盘搜优化

--- js/main.js
+++ js/main.js
@@ -182,7 +182,7 @@ $(function() {
       icon: 'img/pangju-xs.png',
       searchlink: 'https://www.nmme.cc/s/1/',
       searchname: '',
-      color: '#3245df',
+      color: '#e6162d',
       placeholder: '请输入影视名称...'
     }, {
       name: 'baidu',
@@ -511,7 +511,11 @@ class searchHint {
     for (var i = 0; i < msg.s.length; i++) {
       var sk = new Array();
       sk[i] = msg.s[i].replace(/\s*/g, ''); //去掉关键字空格
-      str += '<a href=' + href + '?' + name + '=' + sk[i] + '><li><span>' + (i + 1) + '</span>' + msg.s[i] + '</li></a>';
+      if (href=="https://www.nmme.cc/s/1/") {
+        str += '<a href=' + href + sk[i] + '><li><span>' + (i + 1) + '</span>' + msg.s[i] + '</li></a>';
+      } else {
+        str += '<a href=' + href + '?' + name + '=' + sk[i] + '><li><span>' + (i + 1) + '</span>' + msg.s[i] + '</li></a>';
+      }
     }
     this.list.innerHTML = str;
     if (str) { //有返回才显示#searchlist
